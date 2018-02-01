fd5738fe6db9ce4887293838e21d6b672d409464
Amanjot25 <35691403+Amanjot25@users.noreply.github.com>
2018-02-01 13:28:39 +0100

    suma

---
+++ while.py
@@ -0,0 +1,12 @@
+# coding:utf -8
+num= int(input("Escriba un número: "))
+suma = 0
+i=0
+while num > 0:
+    suma += num
+    num= int(input("Escriba otro número: "))
+if num > i:
+ 	i=i+1
+	suma=suma+num
+print "Es negativo"
+
